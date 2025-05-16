023b58cf4512a922107e5b3563b4c448c159bf2a
Ahmed Gendy <ahmedelgendy1170@gmail.com>
2025-05-16 18:55:32 +0300

    Add project files.

---
+++ Breeze/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Breeze.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product=await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+           await _context.AddAsync(product);
+           await _context.SaveChangesAsync();
+           return product;
+        }
+
+        [HttpPut("{id:int}")]
+
+        public async Task<ActionResult> UpdateProduct(int id,Product product)
+        {
+            if(!productExists(id)) 
+                return BadRequest("Cannot update product");
+
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            if (!productExists(id))
+                return BadRequest("Cannot update product");
+            var product = await _context.Products.FindAsync(id);
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+        private bool productExists(int id)
+        {
+            return _context.Products.Any(x => x.Id == id);
+        }
+    }
+}
---
+++ Breeze/Program.cs
@@ -0,0 +1,22 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); //anything before this line is considered services, anything after this line is considered middleware
+
+
+app.MapControllers();
+
+app.Run();
