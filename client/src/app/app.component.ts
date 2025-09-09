efce1e9031a018fd3a15490e14d0e8e079f7254b
Ahmed Gendy <ahmedelgendy1170@gmail.com>
2025-09-09 23:44:06 +0300

    wip

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   protected title = 'Breeze';
+
+  products:any[]=[];
+  constructor(private http:HttpClient){
+  }
+  baseUrl = 'https://localhost:44376/api/products'
+  ngOnInit(): void {
+    this.http.get<any[]>(this.baseUrl).subscribe({
+      next: (response) => {
+        this.products=response;
+        console.log(this.products);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+}
