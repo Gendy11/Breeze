fa3f560b763435c74e7a860953b29cf450eb9ae8
Ahmed ElGendy <ahmed.elgendy1@vodafone.com>
2025-10-25 21:29:21 +0300

    wip

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ShopComponent } from './features/shop/shop.component';
 import { LoginComponent } from './features/account/login/login.component';
 import { RegisterComponent } from './features/account/register/register.component';
 import { CartComponent } from './features/cart/cart.component';
+import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 
 export const routes: Routes = [
   {path:'',component:ShopComponent},
   {path:'shop',component:ShopComponent},
+  {path:'shop/:id', component:ProductDetailsComponent},
   {path:'cart',component:CartComponent},
   {path:'account/login',component:LoginComponent},
   {path:'account/register',component:RegisterComponent}
--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { CartService } from '../../../core/services/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions,MatIcon],
+  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatIcon, RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
 export class ProductItemComponent {
   @Input() product?:Product;
+  cartService = inject(CartService)
 }
