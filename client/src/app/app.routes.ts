7522181a2e862fec7abd5a5b1be9cdac62c28e48
Ahmed ElGendy <ahmed.elgendy1@vodafone.com>
2025-10-25 19:07:05 +0300

    wip

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ShopComponent } from './features/shop/shop.component';
 import { LoginComponent } from './features/account/login/login.component';
 import { RegisterComponent } from './features/account/register/register.component';
+import { CartComponent } from './features/cart/cart.component';
 
 export const routes: Routes = [
   {path:'',component:ShopComponent},
   {path:'shop',component:ShopComponent},
+  {path:'cart',component:CartComponent},
   {path:'account/login',component:LoginComponent},
   {path:'account/register',component:RegisterComponent}
 
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule, MatFabButton } from '@angular/material/button';
 import { MatButton } from "@angular/material/button";
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AccountService } from '../../core/services/account.service';
 import { BusyService } from '../../core/services/busy.service';
 import {MatProgressBar} from '@angular/material/progress-bar'
@@ -11,7 +11,7 @@ import {MatProgressBar} from '@angular/material/progress-bar'
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule, MatButton, RouterLink,MatProgressBar],
+  imports: [MatIconModule, MatButton, RouterLink, MatProgressBar, RouterLinkActive, MatFabButton],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
