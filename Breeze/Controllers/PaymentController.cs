49f4a4aecabc4b428c5500cc21442d0ea826c1ff
Ahmed ElGendy <ahmed.elgendy1@vodafone.com>
2025-11-09 19:53:12 +0200

    wip

---
+++ Breeze/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace Breeze.Controllers
+{
+    public class PaymentController(IPaymentService paymentService,
+        IGenericRepository<DeliveryMethod> dmRepo) : BaseApiController
+    {
+        [Authorize]
+        [HttpPost("{cartId}")]
+        public async Task<ActionResult<ShoppingCart>> CreateOrUpdatePaymentIntent(string cartId)
+        {
+            var cart = await paymentService.CreateOrUpdatePaymentIntent(cartId);
+            if (cart == null) return BadRequest("Problem with your cart");
+            return Ok(cart);
+        }
+        [HttpGet("delivery-methods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            return Ok(await dmRepo.ListAllAsync());
+        }
+
+    }
+}
