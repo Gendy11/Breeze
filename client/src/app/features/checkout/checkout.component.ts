cf4bd447478e75dea66f0b1e19de18e440bd329f
Ahmed ElGendy <ahmed.elgendy1@vodafone.com>
2025-11-16 19:10:41 +0200

    wip

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, Signal } from '@angular/core';
 import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
-import { MatStepperModule} from '@angular/material/stepper';
+import { MatStepperModule } from '@angular/material/stepper';
 import { RouterLink } from "@angular/router";
 import { MatAnchor, MatButton } from "@angular/material/button";
 import { StripeService } from '../../core/services/stripe.service';
-import { StripeAddressElement } from '@stripe/stripe-js';
+import { StripeAddressElement, StripeAddressElementChangeEvent, StripePaymentElement, StripePaymentElementChangeEvent } from '@stripe/stripe-js';
 import { SnackbarService } from '../../core/services/snackbar.service';
 import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { StepperSelectionEvent } from '@angular/cdk/stepper';
@@ -12,6 +12,9 @@ import { Address } from '../../shared/models/user';
 import { firstValueFrom } from 'rxjs';
 import { AccountService } from '../../core/services/account.service';
 import { CheckoutDeliveryComponent } from "./checkout-delivery/checkout-delivery.component";
+import { CheckoutReviewComponent } from "./checkout-review/checkout-review.component";
+import { CartService } from '../../core/services/cart.service';
+import { CurrencyPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
@@ -23,8 +26,11 @@ import { CheckoutDeliveryComponent } from "./checkout-delivery/checkout-delivery
     MatButton,
     RouterLink,
     MatCheckboxModule,
-    CheckoutDeliveryComponent
-],
+    CheckoutDeliveryComponent,
+    CheckoutReviewComponent,
+    CurrencyPipe,
+    JsonPipe
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
@@ -32,47 +38,82 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   private stripeService = inject(StripeService);
   private snackBar = inject(SnackbarService);
   private accountService = inject(AccountService);
-  addressElement?:StripeAddressElement;
+  cartService = inject(CartService);
+  addressElement?: StripeAddressElement;
+  paymentElement?: StripePaymentElement;
   saveAddress = false;
+  completionStatus = signal<{ address: boolean, card: boolean, delivery: boolean }>({
+    address: false, card: false, delivery: false
+  });
+
 
   async ngOnInit() {
-    try{
+    try {
       this.addressElement = await this.stripeService.createAddressElement();
-      this.addressElement.mount('#address-element')
-    }catch(error:any){
+      this.addressElement.mount('#address-element');
+      this.addressElement.on('change', this.handleAddressChange)
+
+      this.paymentElement = await this.stripeService.createPaymentElement();
+      this.paymentElement.mount('#payment-element')
+      this.paymentElement.on('change',this.handlePaymentChange)
+    } catch (error: any) {
       this.snackBar.error(error.message)
     }
   }
-  async onStepChange(event:StepperSelectionEvent){
-    if(event.selectedIndex ===1){
-      if(this.saveAddress){
+
+  handleAddressChange = (event: StripeAddressElementChangeEvent) => {
+    this.completionStatus.update(state => {
+      state.address = event.complete;
+      return state;
+    })
+  }
+
+  handlePaymentChange = (event: StripePaymentElementChangeEvent) => {
+    this.completionStatus.update(state => {
+      state.card = event.complete;
+      return state;
+    })
+  }
+
+  handleDeliveryChange(event: boolean){
+    this.completionStatus.update(state =>{
+      state.delivery = event;
+      return state;
+    })
+  }
+
+
+
+  async onStepChange(event: StepperSelectionEvent) {
+    if (event.selectedIndex === 1) {
+      if (this.saveAddress) {
         const address = await this.getAddressFromStripeAddress();
         address && firstValueFrom(this.accountService.updateAddress(address))
       }
     }
-    if(event.selectedIndex === 2){
+    if (event.selectedIndex === 2) {
       await firstValueFrom(this.stripeService.createOrUpdatePaymentIntent());
     }
   }
-  private async getAddressFromStripeAddress() : Promise<Address | null> {
+  private async getAddressFromStripeAddress(): Promise<Address | null> {
     const result = await this.addressElement?.getValue();
     const address = result?.value.address;
 
-    if(address){
+    if (address) {
       return {
         line1: address.line1,
         line2: address.line2 || undefined,
         city: address.city,
         country: address.country,
-        postalCode:address.postal_code,
+        postalCode: address.postal_code,
         state: address.state
       }
-    }else{
-      return null;  
+    } else {
+      return null;
     }
   }
 
-  onSaveAddressCheckboxChange(event :MatCheckboxChange){
+  onSaveAddressCheckboxChange(event: MatCheckboxChange) {
     this.saveAddress = event.checked
   }
 
